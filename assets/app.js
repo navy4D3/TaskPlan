96b4572ccdf4ecb43c9fe803a2e75cdfe6d45192
navy <yvanrobert974@gmail.com>
2025-09-10 21:57:52 +0200

    Initialisation docker, webpack, sass

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,29 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './styles/app.scss';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+export function showPopup(divToShow, displayType) {
+    const blurEffect = document.querySelector('.blur-effect');
+
+    blurEffect.style.display = "block";
+    divToShow.style.display = displayType;
+
+    const hidePopupBtn = divToShow.querySelector('.hide-btn');
+
+    if (hidePopupBtn) {
+        hidePopupBtn.addEventListener('click', function() {
+            hidePopup(divToShow);
+        })
+    }
+
+
+}
+export function hidePopup(divToHide) {
+    const blurEffect = document.querySelector('.blur-effect');
+
+    blurEffect.style.display = "none";
+    divToHide.style.display = "none";
+
+    
+}
---
+++ webpack.config.js
@@ -0,0 +1,109 @@
+const Encore = require('@symfony/webpack-encore');
+
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('public/build/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build')
+    // only needed for CDN's or subdirectory deploy
+    //.setManifestKeyPrefix('build/')
+
+    /*
+     * ENTRY CONFIG
+     *
+     * Each entry will result in one JavaScript file (e.g. app.js)
+     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     */
+    .addEntry('app', './assets/app.js')
+
+    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+    .splitEntryChunks()
+
+    // will require an extra script tag for runtime.js
+    // but, you probably want this, unless you're building a single-page app
+    .enableSingleRuntimeChunk()
+
+    /*
+     * FEATURE CONFIG
+     *
+     * Enable & configure other features below. For a full
+     * list of features, see:
+     * https://symfony.com/doc/current/frontend.html#adding-more-features
+     */
+    .cleanupOutputBeforeBuild()
+
+    // Displays build status system notifications to the user
+    // .enableBuildNotifications()
+
+    .enableSourceMaps(!Encore.isProduction())
+    // enables hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+
+    // configure Babel
+    // .configureBabel((config) => {
+    //     config.plugins.push('@babel/a-babel-plugin');
+    // })
+
+    // enables and configure @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = '3.38';
+    })
+
+
+    // enables Sass/SCSS support
+    .enableSassLoader()
+
+    // .addPlugin(new BrowserSyncPlugin(
+    //     {
+    //         proxy: 'https://127.0.0.1:8000', // ton serveur Symfony local
+    //         files: [
+    //             'templates/**/*.twig',
+    //             'assets/**/*.js',
+    //             'assets/**/*.scss'
+    //         ],
+    //         open: false,
+    //     },
+    //     {
+    //         reload: true
+    //     }
+    // ))
+
+    .configureDevServerOptions(options => {
+        options.allowedHosts = 'all';
+        options.liveReload = true;  // Recharge la page en cas de modification
+        // options.hot = true;         // Active le Hot Module Replacement (HMR)
+        options.static = {
+            watch: false
+        }
+        options.client = { 
+            overlay: true,          // Affiche les erreurs dans le navigateur
+        };
+        options.watchFiles = {
+            paths: ['src/**/*.php', 'templates/**/*', 'assets/**/*']
+        }
+    })
+
+    // uncomment if you use TypeScript
+    //.enableTypeScriptLoader()
+
+    // uncomment if you use React
+    //.enableReactPreset()
+
+    // uncomment to get integrity="..." attributes on your script & link tags
+    // requires WebpackEncoreBundle 1.4 or higher
+    //.enableIntegrityHashes(Encore.isProduction())
+
+    // uncomment if you're having problems with a jQuery plugin
+    //.autoProvidejQuery()
+;
+
+module.exports = Encore.getWebpackConfig();
